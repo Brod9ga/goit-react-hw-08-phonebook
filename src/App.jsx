21426d2a1e297362dff565f012a2bcaf7d917711
Brod9ga <gbonik@i.ua>
2023-08-13 21:26:30 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import InputAdornments from "components/LoginForm/LoginForm";
+
+
 export const App = () => {
   return (
     <div>
       <h1>Phonеbook</h1>
-
+<InputAdornments/>
       <h2 className="margin">Contacts</h2>
     </div>
   );
